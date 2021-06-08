4ad01bea8dd4dc065e088390fef0925e7d026011
ricar <ricardojuarezflores01@gmail.com>
2021-06-07 23:18:10 -0500

    Ejercicio 5

--- src/java/uaslp/objetos/escuela/EvaluadorDePromedios.java
+++ src/java/uaslp/objetos/escuela/EvaluadorDePromedios.java
@@ -3,7 +3,6 @@ package uaslp.objetos.escuela;
 import java.util.List;
 
 public class EvaluadorDePromedios {
-
     public double evalua(List<Double> calificaciones){
         double sum = calificaciones.stream().reduce(0.0, Double::sum);
 
